025b460e76b06fced59576611d26c31f0b36e6c0
Samuel Lord <sllord@icloud.com>
2020-12-05 15:52:41 -0800

    Create move-hats.lua

---
+++ move-hats.lua
@@ -0,0 +1,73 @@
+if game.workspace:FindFirstChild("miscparts191001101010") == nil then
+print("--------------------------------------")
+print("               HatMover")
+print("")
+print("This script allows you to move your hats")
+print("--------------------------------------")
+print("")
+print("--------------------------------------")
+print("This scripts was made By TheInvisible_")
+print("--------------------------------------")
+local function align(p,at1,at2,boolers)
+local obj1 = game:GetObjects("rbxassetid://5760223514")
+for i,v in pairs(obj1) do
+v.Parent = p
+v.RigidityEnabled = boolers
+v.Attachment0 = at1
+v.Attachment1 = at2
+end
+local obj2 = game:GetObjects("rbxassetid://5760225829")
+for i,v in pairs(obj2) do
+v.Parent = p
+v.RigidityEnabled = boolers
+v.Attachment0 = at1
+v.Attachment1 = at2
+end
+end
+
+
+
+local folder = Instance.new("Folder",workspace)
+folder.Name = "miscparts191001101010"
+
+
+
+getgenv().makemovers = function(hhandle,pos,ppp1,boolerss,collide)
+local boolv
+if boolerss == nil or boolerss == "" then
+boolv = false
+else
+boolv = boolerss
+end
+if hhandle ~= nil then
+local part11 = ppp1
+local physProperties = PhysicalProperties.new(0, 0, 0, 0, 0)
+hhandle.CustomPhysicalProperties = physProperties
+local RunService = game:GetService("RunService")
+local part = Instance.new("Part",folder)
+part.Transparency = 1
+part.CanCollide = false
+part.Name = "Random Generated part"
+local Attach1 = Instance.new("Attachment",part)
+local Attach2 = Instance.new("Attachment",hhandle)
+align(part,Attach2,Attach1,boolv)
+hhandle:BreakJoints()
+hhandle.SpecialMesh:Destroy()
+local ok = Instance.new("Motor6D",part)
+ok.Part0 = part
+ok.Part1 = part11
+ok.C0 = pos
+hhandle.CanCollide = collide
+hhandle.Parent.Name = "ActiveMoverHat"
+local bruh do
+bruh = game.Players.LocalPlayer.Character.Humanoid.Died:Connect(function()
+part:Destroy()
+ok:Destroy()
+bruh:Disconnect()
+end)
+end
+else
+print("Hat not existing Aborting")
+end
+end
+end
