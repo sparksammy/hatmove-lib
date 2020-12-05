97c330fac8a9127da99fd920709eebbf3e9de977
Samuel Lord <sllord@icloud.com>
2020-12-05 15:54:37 -0800

    Create sample.lua

---
+++ sample.lua
@@ -0,0 +1,3 @@
+local hatname = "" // Replace with hat name thats in explorer
+local hat = game.Players.LocalPlayer.Character[hatname]
+makemovers(hat.Handle,CFrame.new(6,0,0),game.Players.LocalPlayer.Character["Torso"],false,false)
