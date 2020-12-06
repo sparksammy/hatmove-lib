62a853451d62ce96a07b0baa8a386c5478b99d5a
Samuel Lord <sllord@icloud.com>
2020-12-05 16:01:23 -0800

    Update sample.lua

--- sample.lua
+++ sample.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sparksammy/hatmove-lib/main/move-hats.lua", true))()
 local hatname = "" // Replace with hat name thats in explorer
 local hat = game.Players.LocalPlayer.Character[hatname]
 makemovers(hat.Handle,CFrame.new(6,0,0),game.Players.LocalPlayer.Character["Torso"],false,false)
